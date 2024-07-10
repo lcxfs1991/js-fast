0a26320384f696bbecd405f1566bc8b68efb6839
lichengxi12 <lichengxi12@crland.com.cn>
2024-07-10 21:42:52 +0800

    feat: improve

--- snippet/bind.js
+++ snippet/bind.js
@@ -1,11 +1,35 @@
 
-export const bind = (obj, method, ...args) => {
-    return (...newArgs) => {
-        // 判断 method 是否 function
-        if (typeof method !== 'function') {
-            return;
-        }
+Function.prototype.myBind = function(context) {
+    if (typeof this !== 'function') {
+        throw new Error('myBind is not defined.');
+    }
 
-        method.apply(obj, [...args, ...newArgs]);
-    }; 
+    let fn = this;
+    let ctx = context;
+
+    return function func() {
+        let args = arguments;
+        let result;
+
+        return fn.apply(ctx, args);
+    }
+}
+
+
+const obj = {
+    name: 'abc',
 };
+
+const obj1 = {
+    name: 'cde',
+};
+
+function log(d, e) {
+    console.log(this.name, d, e);
+}
+
+const log1 = log.bind(obj);
+log1.bind(obj1)('d', 'e');
+
+const log2 = log.myBind(obj);
+log2.bind(obj1)('d', 'e');
